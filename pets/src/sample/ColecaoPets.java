0bb51f9a15fd0d1d08b3615c734e251cb39d094b
JuanFSR <42705233+JuanFSR@users.noreply.github.com>
2021-05-12 18:01:28 -0300

    Add files via upload

---
+++ pets/src/sample/ColecaoPets.java
@@ -0,0 +1,23 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class ColecaoPets {
+    private ArrayList<virtualPet> todosPets;
+
+    //Construtor de ColecaoPets
+    public ColecaoPets(){
+        todosPets = new ArrayList<virtualPet>();
+    }
+
+    //método para adicionar novos pets
+    public void AdicionarPet(virtualPet p){
+        if(p != null){
+            todosPets.add(p);
+        }
+    }
+
+     public ArrayList<virtualPet> getPets(){
+        return todosPets;
+     }
+}
--- pets/src/sample/Main.java
+++ pets/src/sample/Main.java
@@ -7,17 +7,50 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //Agora vamos criar uma cena
+    private static Scene sceneSample, sceneListarPets;
+    private static Stage primaryStage;
+
+    public static ColecaoPets colecaoPets;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage stage) throws Exception{
+        //Guarda a primeira janela (sample - será usada para cadastrar Pets)
+        FXMLLoader sample = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader listar_pets = new FXMLLoader(getClass().getResource("listar_pets.fxml"));
+
+        primaryStage = stage;
+
+        //Definir um título para o palco
+        primaryStage.setTitle("Navegação entre janelas");
+
+        //Agora definimos uma janela inicial
+        Parent parentSample = sample.load();
+        Parent parentListarPets = listar_pets.load();
+
+        //Será uma nova cena
+//        sceneSample = new Scene(parentSample, 600, 400);
+        sceneListarPets = new Scene(parentListarPets, 600, 400);
+
+        //Inicia o Stage como a janela sample
+        primaryStage.setScene(new Scene(parentSample));
         primaryStage.show();
     }
 
+    //método para que vejamos uma tela ou outra
+    public static void mudaTela(int opcao){
+        switch (opcao){
+            case 1:
+//                primaryStage.setScene(sceneSample);
+                break;
+            case 2:
+                primaryStage.setScene(sceneListarPets);
+                break;
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        colecaoPets = new ColecaoPets();
         launch(args);
     }
 }
